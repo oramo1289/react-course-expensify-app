6d710393cd4f789b0755b5111537eb76fb695287
Oscar Eduardo Ramo Hilton <oramo1289@gmail.com>
2019-07-17 16:53:39 -0500

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -7,10 +7,9 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
-
 const store = configureStore();
 
-
+console.log('testing');
 const jsx = (
     <Provider store={store}>
         <AppRouter/>

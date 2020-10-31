9bec00ce7af0cfcde860fbdbae86f66c787f8938
oramo1289 <oramo1289@gmail.com>
2020-10-30 21:03:24 -0600

    expensify test

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,4 +1,5 @@
 const add = (a, b) =>  a + b;
+const generateGreeting = (name) => `hello ${name}`;
 
 test('should add two numbers', () => {
     const result = add(3, 4);
@@ -6,4 +7,12 @@ test('should add two numbers', () => {
     //     throw new Error(`you add  4 and 3 the resul was ${result}`)
     // }
     expect(result).toBe(7)
+});
+
+test('generate greeting', () => {
+    const result = generateGreeting('oscar');
+    // if (result !== 7) {
+    //     throw new Error(`you add  4 and 3 the resul was ${result}`)
+    // }
+    expect(result).toBe('hello oscar');
 });
